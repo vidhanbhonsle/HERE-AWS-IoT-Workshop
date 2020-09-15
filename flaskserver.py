f46cd993964cbf1967c7d2237312415684a155e3
Vidhan Bhonsle <vidhanbhonsle@gmail.com>
2020-09-15 11:01:06 +0530

    update

---
+++ flaskserver.py
@@ -0,0 +1,49 @@
+from flask import Flask,render_template
+from flask_mqtt import Mqtt
+
+import ssl
+import urllib.request as request
+# import threading
+app  = Flask(__name__)
+
+app.config['MQTT_BROKER_URL'] = 'a2e941l3hmci8-ats.iot.ap-south-1.amazonaws.com'
+app.config['MQTT_BROKER_PORT'] = 8883
+app.config['MQTT_CLIENT_ID'] = "geocode"
+app.config['MQTT_KEEPALIVE'] = 60
+app.config['MQTT_TLS_ENABLED'] = True
+app.config['MQTT_TLS_CA_CERTS'] = "root-CA.crt"
+app.config['MQTT_TLS_CERTFILE'] = "geocode.cert.pem"
+app.config['MQTT_TLS_KEYFILE'] = "geocode.private.key"
+app.config['MQTT_TLS_CIPHERS'] = None
+app.config['MQTT_TLS_CERT_REQS'] = ssl.CERT_REQUIRED
+app.config['MQTT_TLS_VERSION'] = ssl.PROTOCOL_TLSv1_2
+
+mqtt = Mqtt(app)
+
+lat = 0
+lng = 0
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    print ('connected and waiting for msg')
+    mqtt.subscribe('iot')
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, msg):
+    global lat, lng
+    data = eval(msg.payload.decode())
+    lat = data['latitude']
+    lng = data['longitude']
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+@app.route('/getlatlng')
+def getlatlng_page():
+    global lat, lng
+    print("called ajax : " , str(lat)+','+str(lng))
+    return str(lat)+','+str(lng)
+
+if __name__ == '__main__':
+    app.run( use_reloader=False, debug=True)
+
---
+++ geocode_data_publish.py
@@ -0,0 +1,56 @@
+import json
+import paho.mqtt.client as paho
+import os
+import socket
+import ssl
+from time import sleep
+import random
+from random import uniform
+
+connflag = False
+
+#print(ssl.OPENSSL_VERSION)
+
+def on_connect(client, userdata, flags, rc):
+    global connflag
+    connflag = True
+    print("Connection returned result: " + str(rc) )
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+
+mqttc = paho.Client()
+
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+latitude = 12.9716
+longitude = 77.5946
+
+awshost = "a2e941l3hmci8-ats.iot.ap-south-1.amazonaws.com"
+awsport = 8883
+clientId = "geocode"
+thingName = "geocode"
+caPath = "root-CA.crt"
+certPath = "geocode.cert.pem"
+keyPath = "geocode.private.key"
+
+mqttc.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
+
+mqttc.connect(awshost, awsport, keepalive=60)
+
+mqttc.loop_start()
+
+while 1==1:
+    sleep(5)
+    if connflag == True:
+        new_lat = latitude+random.random()/100
+        new_lon = longitude+random.random()/100
+        
+        payload = json.dumps({'latitude':new_lat, 'longitude':new_lon})
+        mqttc.publish("iot", payload, qos=0)
+        print("msg sent: " + "%s" % payload )
+    else:
+        print("waiting for connection...")
